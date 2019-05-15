9ae260da9cffe458b2c5d42576167a86e9cbe521
RicsiToth <50337780+RicsiToth@users.noreply.github.com>
2019-05-15 21:41:21 +0200

    Update Hra.java

--- src/maze/Hra.java
+++ src/maze/Hra.java
@@ -25,6 +25,7 @@ public class Hra {
         REMOVESHAFTFROMFLOOR,
         RESETVISITED,
         RESETFLOOR,
+        NOTHING,
     }
     
     public Hra(int rows, int columns, int floors, ArrayList<String> string, ContextMenu tools){
@@ -32,7 +33,7 @@ public class Hra {
         this.columns = columns;
         this.floors = floors;
         labels = new ArrayList<>();
-        
+        status = SelectedTool.NOTHING;
         int index = 0;
         for(int i = 0; i < this.floors; i++){
             labels.add(new ArrayList<>());
@@ -53,58 +54,59 @@ public class Hra {
                     } else {
                         label.setId("Free");
                     }
-                    int row = j; 
-                    int column = k;
-                    int floor = i;
-                    label.setOnMouseClicked((ActionEvent) -> {
-                        MouseButton btn = ActionEvent.getButton();
-                        if(btn == MouseButton.PRIMARY){
-                            switch(status){
-                                case SEARCH:
-                                    colourIt(floor, row, column);
-                                    break;
-                                case ADDWALL:
-                                    if(!label.getId().equals("Shaft")){
-                                        label.setId("Wall");
-                                    }
-                                    break;
-                                case ADDRANDOMWALLS:
-                                    generateWalls(floor);
-                                    break;
-                                case REMOVEWALL:
-                                    if(label.getId().equals("Wall")){
-                                        label.setId("Free");
-                                    }
-                                    break;
-                                case ADDSHAFT:
-                                    placeShaft(row, column);
-                                    break;
-                                case ADDSHAFTTONEXTFLOOR:
-                                    addShaftToNextFloor(row, column, floor);
-                                    break;
-                                case REMOVESHAFT:
-                                    if(label.getId().equals("Shaft")){
-                                        removeShaft(row, column);
-                                    }
-                                    break;
-                                case REMOVESHAFTFROMFLOOR:
-                                    if(label.getId().equals("Shaft")){
-                                        label.setId("Free");
-                                    }
-                                    break;
-                                case RESETVISITED:
-                                    resetVisited();
-                                    break;
-                                case RESETFLOOR:
-                                    resetFloor(floor);
-                                    break;
-                            }
-                        }
-                    });
+                    createLabel(label, i, j, k);
                     labels.get(i).get(j).add(label);
                 }
             }
         }
+    }
+    
+    private void createLabel(Label label, int floor, int row, int column){
+        label.setOnMouseClicked((ActionEvent) -> {
+            MouseButton btn = ActionEvent.getButton();
+            if(btn == MouseButton.PRIMARY){
+                switch(status){
+                    case SEARCH:
+                        colourIt(floor, row, column);
+                        break;
+                    case ADDWALL:
+                        if(!label.getId().equals("Shaft")){
+                            label.setId("Wall");
+                        }
+                        break;
+                    case ADDRANDOMWALLS:
+                        generateWalls(floor);
+                        break;
+                    case REMOVEWALL:
+                        if(label.getId().equals("Wall")){
+                            label.setId("Free");
+                        }
+                        break;
+                    case ADDSHAFT:
+                        placeShaft(row, column);
+                        break;
+                    case ADDSHAFTTONEXTFLOOR:
+                        addShaftToNextFloor(row, column, floor);
+                        break;
+                    case REMOVESHAFT:
+                        if(label.getId().equals("Shaft")){
+                            removeShaft(row, column);
+                        }
+                        break;
+                    case REMOVESHAFTFROMFLOOR:
+                        if(label.getId().equals("Shaft")){
+                            label.setId("Free");
+                        }
+                        break;
+                    case RESETVISITED:
+                        resetVisited();
+                        break;
+                    case RESETFLOOR:
+                        resetFloor(floor);
+                        break;
+                }
+            }
+        });
     }
    
     private void colourIt(int floor, int row, int column){
@@ -243,7 +245,7 @@ public class Hra {
             out.println();
         }
     }
-    
+        
     public void setSearch(){
         status = SelectedTool.SEARCH;
     }
@@ -282,5 +284,104 @@ public class Hra {
     
     public void setResetFloor(){
         status = SelectedTool.RESETFLOOR;
+    }
+    
+    public int addFloor(ContextMenu tools){
+        status = SelectedTool.NOTHING;
+        if((floors+1) < 11){
+            floors++;
+            labels.add(new ArrayList<>());
+            for(int i = 0; i < rows; i++){
+                labels.get(floors-1).add(new ArrayList<Label>());
+                for(int j = 0; j < columns; j++){
+                    Label label = new Label();
+                    label.setContextMenu(tools);
+                    label.setId("Free");
+                    createLabel(label, floors-1, i, j);
+                    labels.get(floors-1).get(i).add(label);
+                }
+            }
+            return floors-1;
+        } else {
+            return -1;
+        }
+    }
+    
+    public int removeFloor(){
+        status = SelectedTool.NOTHING;
+        if(floors-1 > 0){
+            labels.get(floors-1).clear();
+            floors--;
+            return floors;
+        } else {
+            return -1;
+        }
+    }
+    
+    public boolean addRow(ContextMenu tools){
+        status = SelectedTool.NOTHING;
+        if(rows+1 < 101){
+            rows++;
+            for(int i = 0; i < floors; i++){
+                labels.get(i).add(new ArrayList<>());
+                for(int j = 0; j < columns; j++){
+                    Label label = new Label();
+                    label.setContextMenu(tools);
+                    label.setId("Free");
+                    createLabel(label, i, rows-1, j);
+                    labels.get(i).get(rows-1).add(label);
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean removeRow(){
+        status = SelectedTool.NOTHING;
+        if(rows-1 >= 3){
+            for(int i = 0; i < floors; i++){
+                labels.get(i).get(rows-1).clear();
+            }
+            rows--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean addColumn(ContextMenu tools){
+        status = SelectedTool.NOTHING;
+        if(columns+1 < 101){
+            columns++;
+            for(int i = 0; i < floors; i++){
+                for(int j = 0; j < rows; j++){
+                    Label label = new Label();
+                    label.setContextMenu(tools);
+                    label.setId("Free");
+                    createLabel(label, i, j, columns-1);
+                    labels.get(i).get(j).add(label);
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean removeColumn(){
+        status = SelectedTool.NOTHING;
+        if(columns-1 >= 3){
+            for(int i = 0; i < floors; i++){
+                for(int j = 0; j < rows; j++){
+                    labels.get(i).get(j).remove(columns-1);
+                }
+            }
+            columns--;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
